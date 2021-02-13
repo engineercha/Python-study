db42387924ce28997c12f852378b08e505719b2d
Cha Subin <77966598+engineercha@users.noreply.github.com>
2021-02-13 17:08:46 +0900

    day7 *집계 시간과 국외 현황을 크롤링을 통해 받음. *반복문을 통한 코드 단순화 진행 중.

--- pj_covid19.py
+++ pj_covid19.py
@@ -1,3 +1,4 @@
+import re
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -6,29 +7,25 @@ print(time.localtime())
 print("코로나바이러스감염증-19 (COVID-19)")
 
 covid19_url = ("https://search.naver.com/search.naver?sm=tab_sug.top&where=nexearch&query=%EA%B5%AD%EB%82%B4+%EC%BD%94%EB%A1%9C%EB%82%98+%ED%99%95%EC%A7%84%EC%9E%90+%ED%98%84%ED%99%A9&oquery=%ED%81%AC%EB%A1%A4%EB%A7%81+for+number&tqi=huh4Msp0JXossExdgw4ssssstHs-369932&acq=%ED%99%95%EC%A7%84%EC%9E%90+%ED%98%84%ED%99%A9&acr=1&qdt=0")
-raw = requests.get(covid19_url)
-soup = BeautifulSoup(raw.text, 'html.parser')
+res = requests.get(covid19_url)
+soup = BeautifulSoup(res.text, 'html.parser')
 
-# box1 = soup.find('div', {'class' : 'csp_infoCheck_area._togglor_root'})
-times = soup.find('span', {'class' : '_update_time'})
-print(times)
+times = soup.find_all('span', {'class':'_update_time'})
+print(times[1].get_text(),"기준")
 
 box = soup.find('div', {'class' : 'status_info'})
-numbers = box.find_all('p')
-i=0
+numbers = box.findAll('p')
+box2 = soup.find('div', {'class' : 'status_info abroad_info'})
+numbers_ex = box2.findAll('p')
 
-for number in numbers:
-    if i==0:
-        cases_in=number.text
-    elif i==1:
-        test_in=number.text
-    elif i==2:
-        lift_in=number.text
-    else: deaths_in=number.text
-    i+=1
 
-# cases_ex = 107778431
-# deaths_ex = 2368525
+cases_in=numbers[0].text
+test_in=numbers[1].text
+lift_in=numbers[2].text
+deaths_in=numbers[3].text
+
+cases_ex=numbers_ex[0].text
+deaths_ex=numbers_ex[1].text
 
 print("\n<국내현황>")
 print("확진환자", cases_in)
@@ -36,20 +33,46 @@ print("검사중  ", test_in)
 print("격리해제", lift_in)
 print("사망자  ", deaths_in)
 
-# print("\n<세계현황>")
-# print("확진환자", cases_ex)
-# print("사망자  ", deaths_ex)
+print("\n<세계현황>")
+print("확진환자", cases_ex)
+print("사망자  ", deaths_ex)
 
-# print("\n세계 대비 국내 확진환자 비율:",cases_in/cases_ex*100,"%")
-# print("세계 대비 국내   사망자 비율:",deaths_in/deaths_ex*100,"%")
 
-# drate_in = deaths_in/cases_in*100
-# drate_ex = deaths_ex/cases_ex*100
-# print('\n국내 치명률: %.3f'%drate_in,"%") 
-# print('세계 치명률: %.3f'%drate_ex,"%")
+C=re.findall('\d',cases_in)
+rtc=""
+for n in C:
+    rtc+=n
+cases_in=int(rtc)
 
-if drate_in > drate_ex:
-    print("국내 치명률은 세계 치명률 보다 약",drate_in - drate_ex,"% 높습니다")
-elif drate_in < drate_ex:
-    print("국내 치명률은 세계 치명률 보다 약",drate_ex - drate_in,"% 낮습니다.")
+D=re.findall('\d',deaths_in)
+rtd=""
+for n in D:
+    rtd+=n
+deaths_in=int(rtd)
+
+C2=re.findall('\d',cases_ex)
+rtc2=""
+for n in C2:
+    rtc2+=n
+cases_ex=int(rtc2)
+
+D2=re.findall('\d',deaths_ex)
+rtd2=""
+for n in D2:
+    rtd2+=n
+deaths_ex=int(rtd2)
+
+
+print("\n세계 대비 국내 확진환자 비율:",cases_in/cases_ex*100,"%")
+print("세계 대비 국내   사망자 비율:",deaths_in/deaths_ex*100,"%")
+
+rate_in = deaths_in/cases_in*100
+rate_ex = deaths_ex/cases_ex*100
+print('\n국내 치명률: %.3f'%rate_in,"%") 
+print('세계 치명률: %.3f'%rate_ex,"%")
+
+if rate_in > rate_ex:
+    print("국내 치명률은 세계 치명률 보다 약",rate_in - rate_ex,"% 높습니다")
+elif rate_in < rate_ex:
+    print("국내 치명률은 세계 치명률 보다 약 %.3f"%(rate_ex - rate_in),"% 낮습니다.")
 else: print("국내 치명률은 세계 치명률과 같습니다.")
