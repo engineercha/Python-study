f70e95b55aeeb7589e094fa5221c05cf419738e1
Cha Subin <77966598+engineercha@users.noreply.github.com>
2021-02-21 23:50:59 +0900

    Test

---
+++ pj_baseball_game.py
@@ -0,0 +1,34 @@
+import random
+
+true = random.randrange(100,999,1)
+
+strike = 0
+ball = 0
+
+player1 = input("100의 자리수: ")
+player2 = input("10의 자리수: ")
+player3 = input("1의 자리수: ")
+
+if player1 == true//100:
+    strike +=1
+elif: player1 == true//10:
+    ball +=1
+elif: player1 == true//1
+    ball +=1
+else: pass
+        
+if player2 == true//100:
+    strike +=1
+elif player2 == true//10:
+    ball +=1
+elif: player2 == true//1
+    ball +=1
+else: pass
+
+if player3 == true//100:
+    strike +=1
+elif player3 == true//10:
+    ball +1
+ or player3 == true//1:
+    ball +=1
+    else: pass
