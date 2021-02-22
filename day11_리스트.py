1dd7cce51dedccc255b7027a7219a4d59da99032
Cha Subin <77966598+engineercha@users.noreply.github.com>
2021-02-22 23:22:38 +0900

    day14

--- day11_리스트.py
+++ day11_리스트.py
@@ -20,3 +20,20 @@ del year_lst[1]         #1번째 인덱스를 제거
 print(year_lst)
 year_lst.remove(2)      #첫번째 2를 제거
 print(year_lst)
+
+
+#오늘의 문제
+#1
+numbers=[]
+sum=0
+for i in range(7):
+    numbers.append(input("정수를 입력하세요: "))
+    sum+=int(numbers[i])
+avg=sum/7
+print("평균: %f"%avg)
+
+#2
+numbers=[]
+for i in range(7):
+    numbers.append(int(input("정수를 입력하세요: ")))
+print("평균:", sum(numbers)/7)
