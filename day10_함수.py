e05242b39b035dd24ddbfe0e196e1eee285813b8
Cha Subin <77966598+engineercha@users.noreply.github.com>
2021-02-28 00:02:06 +0900

    day13, day18, day19

--- day10_함수.py
+++ day10_함수.py
@@ -22,4 +22,24 @@ def getmax_idx(x):
     
 numbers=[random.randrange(1,2021) for i in range(0,10)]
 print(numbers,"중")
-print("최대값은 %d"%getmax_idx(numbers)+"번째에 있습니다.")
+print("최대값은 %d"%getmax_idx(numbers)+"번째에 있습니다.",end='\n\n')
+
+#ver2
+import random
+def getmax_idx(x):
+
+    max_value=max(x)
+    max_idx=[]
+
+    for i in range(1,len(x)):
+        if x[i]==max_value:
+            max_idx.append(i)
+    return max_idx
+
+numbers=[random.randrange(1,10) for i in range(0,20)]
+print(numbers)
+print("max = %d = "%max(numbers),end='')
+for i in getmax_idx(numbers):
+    if i==getmax_idx(numbers)[-1]:    #마지막 인덱스는 , 출력하지 않음
+        print("numbers[%d]"%i)
+    else: print("numbers[%d]"%i,end=', ')
