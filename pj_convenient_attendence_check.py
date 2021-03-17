f6657e31d8b707e78d64bbf9d3444849a9b84b19
chasubin <cha_subin@naver.com>
2021-03-17 23:58:30 +0900

    day20
    selenium 사용함.
    아이디, 비밀번호 입력으로 로그인창>캠퍼스창으로 넘어가짐.
    아직 사용자 이름 크롤링하는 건 실패.

--- pj_convenient_attendence_check.py
+++ pj_convenient_attendence_check.py
@@ -1,33 +1,49 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import time
+import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 s=requests.Session()
 
 login_data={
-    'id': '~~~여러분의 아이디~~~',
-    'passwor' : '~~~여러분의 비번~~~'
+    'usr': '학번',
+    'pwd' : '유레카 비번'
 }
 
 log_url=('https://sso.ewha.ac.kr/SSO_IDP/swift/sso/loginForm.jsp?RSP=cyber.ewha.ac.kr&RelayState=%2FmakeSsoToken_php.jsp%3FretURL%3D%252F')
 main_url=('https://cyber.ewha.ac.kr/')
 
+driver=webdriver.Chrome(ChromeDriverManager().install())
+driver.get(log_url)
 
 def send_data(url, login):
     resp=s.post(url, data=login)
-    print(resp)                     #HTTP 상태 코드 출력, 200=요청성공, 404=실패, 403=금지
+    print(resp)                 #HTTP 상태 코드 출력함 (200=요청성공, 404=실패)
 
 def check_login(url):
     res=s.get(url)
-    checker=bs(res.text, 'html.parser')
-    # name=checker.find_all('li', {'class':'nav-item.user_department.hidden-xs'})
-    # results=checker.find_all('li')
-
-    box = checker.find('nav', {'class':'navbar_navbar-default_navbar-fixed-top_headroom'})
-    name = box.findAll('li')
-    print(name)
-
-    if '~~~여러분의 이름~~~' in str(name): print("로그인 성공")
+    time.sleep(2)
+    soup=bs(res.text, 'html.parser')
+    name=[]
+    for tag in soup.select('h4.popover-body'):
+        name.append(tag.text)
+        print(tag.text)
+    if '이름' in name: print("로그인 성공")
     else: print("로그인 실패")
+
+
+usr="학번"
+pwd="유레카 비번"
+
+elem=driver.find_element_by_id("login_id")
+elem.send_keys(usr)
+elem=driver.find_element_by_id("usr_pwd")
+elem.send_keys(pwd)
+elem.send_keys(Keys.RETURN)                 #엔터 누르게 함
+time.sleep(3)
 
 send_data(log_url,login_data)
 check_login(main_url)
